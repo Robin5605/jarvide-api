37a10f3b24f7b098fdb4ea9a5558edcf6efa9927
CaedenPH <caedenperelliharris@gmail.com>
2022-01-13 18:32:39 +0000

    auth homepage

--- auth.js
+++ auth.js
@@ -1,16 +1,24 @@
 // const { MasterApiKey } = require('./config.json');
-
+const path = require("path")
+  
 module.exports = (req, res, next) => {
     const MasterApiKey = "a"
     const { ['api-key']: apiKey } = req.headers;
-    const endpoint = res['_parsedUrl']['_raw']
-    
+    const endpoint = req.path
+
     
     if (endpoint === "/") {
-
+      res.redirect("https://github.com/Robin5605/jarvide-api/blob/master/README.md")
+      return
     }
+
+
     if(apiKey === MasterApiKey) {
         next();
+    } else if ( endpoint === "/get" ) {
+        res.send({
+            "ERROR": "Invalid authentication token or none provided at all.",
+        });
     } else {
         res.send({
             "ERROR": "Invalid authentication token or none provided at all.",
---
+++ prisma/migrations/20220113003428_github/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Github" (
+    "userID" INTEGER NOT NULL,
+    "token" INTEGER NOT NULL,
+
+    CONSTRAINT "Github_pkey" PRIMARY KEY ("userID")
+);
